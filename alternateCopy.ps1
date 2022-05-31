c49c5054fae0a4f79c0660f6c3369aba14d88c9f
Shohan20lac <r.shohan12@gmail.com>
2022-05-31 15:12:23 +0600

    Create alternateCopy.ps1

---
+++ alternateCopy.ps1
@@ -0,0 +1,13 @@
+﻿$Source = "C:\Users\rshoh\Downloads\processed_person_data-20220531T070209Z-001\processed_person_data\4700\obj"
+
+$Destination = "C:\Users\rshoh\Downloads\processed_person_data-20220531T070209Z-001\400"
+
+$Files = Get-ChildItem -Recurse -Path $Source -File | ForEach-Object {$_.FullName }
+
+for ($i=0; $i -lt 800; $i+=4){
+    Copy-Item $FIles[$i] $Destination
+}
+
+for ($i=1; $i -lt 800; $i+=4){
+   Copy-Item $FIles[$i] $Destination
+}
