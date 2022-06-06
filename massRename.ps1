0d1563c84e94c59d139a61ff3d14c7cfb703bd54
Shohan20lac <r.shohan12@gmail.com>
2022-06-06 15:17:45 +0600

    Mass Rename template saved
    renames all files in $allpics folder into sequential filenames, ranging from 0 to $num

---
+++ massRename.ps1
@@ -0,0 +1,10 @@
+﻿$allpics = Get-ChildItem -Path 'C:\Users\rshoh\OneDrive\alter_sense\extract_data_shohan' -recurse
+
+for ($num = 0 ; $num -le 782 ; $num++){
+    $newname = $num.toString()
+
+    $allpics[$num]
+    
+    Rename-Item $allpics[$num] -NewName $newname 
+    
+}
